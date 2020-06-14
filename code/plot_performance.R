b75049eb3ce49ec2831d571c1180d29f2c3e1a98
YuLin-Cheng <108753209@nccu.edu.tw>
2020-06-15 01:02:23 +0800

    correct x-axis label on regression_validation.png and related files

--- code/plot_performance.R
+++ code/plot_performance.R
@@ -1,5 +1,5 @@
-png(file="XGBoost_validation.png")
-data <- read.csv(paste0(getwd(), "/performance_XGBoost.csv"))
+png(file="/docs/XGBoost_validation.png")
+data <- read.csv(paste0(getwd(), "/docs/performance_XGBoost.csv"))
 x = data[,1]
 y = data[,3]
 plot(x, y, type = "l",main="XGBoost cross validation",xlab="Round",ylab="Test error")# ylim = c(-5, 20))
@@ -7,12 +7,12 @@ dev.off()
 
 rm(list = ls())
 
-png(file="Regression_validation.png")
-data <- read.csv(paste0(getwd(), "/performance_glm.csv"))
+png(file="/docs/Regression_validation.png")
+data <- read.csv(paste0(getwd(), "/docs/performance_glm.csv"))
 x = data[,1]
 y = data[,2]
 y2 = data[,3]
-plot(x, y, type = "b",main="Linear regression validation", xlab="Fold",ylab="Error",ylim=c(0.65,0.92))# ylim = c(-5, 20))
+plot(x, y, type = "b",main="Linear regression validation", xlab="Fold",ylab="Accurary",ylim=c(0.65,0.92))# ylim = c(-5, 20))
 lines(x,y2,type="b",col="red")
 legend(8, 0.91, legend=c("Public", "Private"),
        col=c("black", "red"), lty=1:1, cex=0.8)
